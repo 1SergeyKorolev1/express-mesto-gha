84c326ca447c38017ebc04200db66959da71a7d7
Sergey Korolev <bongrichard@rambler.ru>
2022-11-23 02:07:58 +0300

    11

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,12 @@ const {
 
 // Возвращаем всех пользователей
 router.get('/', getUsers);
+// Возвращаем инфу пользователя
+router.get('/me', getUserMe);
 // Возвращаем пользователя по ид
 router.get('/:userId', getUser);
 // Обновляем профиль
 router.patch('/me', patchUser);
-// Возвращаем инфу пользователя
-router.get('/me', getUserMe);
 // Обновляем Аватар
 router.patch('/me/avatar', patchAvatar);
 
